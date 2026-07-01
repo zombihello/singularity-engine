be5e57aa2301559eb63491bdcebf037b838cda10
yehor.pohuliaka <yehor.pohuliaka@brokensingularity.com>
2026-07-01 15:02:37 +0300

    CIT-26 Improved SMDL format

--- src/public/utils/smdldoc/smdl_compiled_doc.h
+++ src/public/utils/smdldoc/smdl_compiled_doc.h
@@ -5,29 +5,39 @@
 #include "modelsystem/modeltypes.h"
 
 //-----------------------------------------------------------------------------
+// Types to initialize SMDL data
+//-----------------------------------------------------------------------------
+enum smdlInitialDataFlag_t
+{
+	SMDL_INITIALDATA_FLAG_NONE				  = 0,
+	SMDL_INITIALDATA_FLAG_REFERENCE_VERTICES  = BIT( 0 ),
+	SMDL_INITIALDATA_FLAG_REFERENCE_INDICES	  = BIT( 1 ),
+	SMDL_INITIALDATA_FLAG_REFERENCE_SURFACES  = BIT( 2 ),
+	SMDL_INITIALDATA_FLAG_REFERENCE_MATERIALS = BIT( 3 )
+};
+
+struct smdlInitialData_t
+{
+	modelVertexType_t	  vertexType;
+	modelIndexType_t	  indexType;
+	uint32				  sizeVertices;
+	uint32				  sizeIndices;
+	uint32				  numSurfaces;
+	uint32				  numMaterials;
+	const byte*			  pVertices;
+	const byte*			  pIndices;
+	const modelSurface_t* pSurfaces;
+	const char**		  pMaterials;
+};
+
+//-----------------------------------------------------------------------------
 // Helper for work with compiled SMDL (Singularity Model) files
 //-----------------------------------------------------------------------------
-struct smdlVertex_t
-{
-	vector4_t position;
-	vector2_t texCoord;
-	vector4_t normal;
-	vector4_t tangent;
-	vector4_t binormal;
-};
-
-struct smdlSurface_t
-{
-	uint32 materialID;
-	uint32 baseVertexIndex;
-	uint32 firstIndex;
-	uint32 numIndices;
-};
-
 class CSMDLCompiledModelDoc
 {
 public:
 	CSMDLCompiledModelDoc();
+	~CSMDLCompiledModelDoc();
 
 	// Load and save a file
 	// NOTE: For use Tier1 must be connected by ConnectTier1
@@ -35,19 +45,40 @@ public:
 	bool SaveFile( const char* pPath );
 	void Clear();
 
-	void SetData( const eastl::vector<smdlVertex_t>& vertices, const eastl::vector<uint32>& indices, const eastl::vector<smdlSurface_t>& surfaces, const eastl::vector<eastl::string>& materials );
-	void SetData( eastl::vector<smdlVertex_t>& vertices, eastl::vector<uint32>& indices, eastl::vector<smdlSurface_t>& surfaces, eastl::vector<eastl::string>& materials );
+	void SetData( const smdlInitialData_t& initialData, uint8 flags = SMDL_INITIALDATA_FLAG_NONE );
 
-	const eastl::vector<smdlVertex_t>&	GetVertices() const;
-	const eastl::vector<uint32>&		GetIndices() const;
-	const eastl::vector<smdlSurface_t>& GetSurfaces() const;
-	const eastl::vector<eastl::string>& GetMaterials() const;
+	modelVertexType_t	  GetVertexType() const;
+	modelIndexType_t	  GetIndexType() const;
+	uint32				  GetSizeVertices() const;
+	uint32				  GetSizeIndices() const;
+	uint32				  GetNumSurfaces() const;
+	uint32				  GetNumMaterials() const;
+	const byte*			  GetVertices() const;
+	const byte*			  GetIndices() const;
+	const modelSurface_t* GetSurfaces() const;
+	const char**		  GetMaterials() const;
 
 private:
-	eastl::vector<smdlVertex_t>	 vertices;
-	eastl::vector<uint32>		 indices;
-	eastl::vector<smdlSurface_t> surfaces;
-	eastl::vector<eastl::string> materials;
+	enum smdlDataFlag_t
+	{
+		SMDL_DATA_FLAG_NONE			 = 0,
+		SMDL_DATA_FLAG_OWN_VERTICES	 = BIT( 0 ),
+		SMDL_DATA_FLAG_OWN_INDICES	 = BIT( 1 ),
+		SMDL_DATA_FLAG_OWN_SURFACES	 = BIT( 2 ),
+		SMDL_DATA_FLAG_OWN_MATERIALS = BIT( 3 )
+	};
+
+	modelVertexType_t	  vertexType;
+	modelIndexType_t	  indexType;
+	uint8				  flags;
+	uint32				  sizeVertices;
+	uint32				  sizeIndices;
+	uint32				  numSurfaces;
+	uint32				  numMaterials;
+	const byte*			  pVertices;
+	const byte*			  pIndices;
+	const modelSurface_t* pSurfaces;
+	const char**		  pMaterials;
 };
 
 #include "utils/smdldoc/smdl_compiled_doc.inl"
